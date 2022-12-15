25470c39c21a81082beab872a056137f3bac4173
rez433 <r3zaee@gmail.com>
2022-12-15 09:05:00 +0100

    Update server.js

--- me/jsramverk-backend/text-editor-backend/server.js
+++ me/jsramverk-backend/text-editor-backend/server.js
@@ -3,19 +3,6 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 1337;
 const {app} = require("./app");
 
-
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
-    
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
-//     })
-// } else {
-//     app.get('/', (req, res) => {
-//         res.send('set NODE_ENV to production!')
-//     })
-// }
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(port, () => {
